efa3c8134e9b4ee7544204bd41e4f557f887dded
mysteriou <elio007@hotmail.fr>
2022-04-07 05:58:47 +0200

    ajout fichier ./js.add_balise.js

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
  
   </head>
 
+  
 <div>
 <div>
 
@@ -43,79 +44,13 @@ var id = 0;
 
 var souligne = 0;
 
-function add_balise(balise){
-
-    id += 1;
-
-    var idbutton = "idbutton"+id;
-
-    var idinput = "newID"+id;
-
-    var finbalise = "fin"+balise;
-
-    var $txt = $("#t"); 
-    
-    var caretPos = $txt[0].selectionStart;
-    
-    var textAreaTxt = $txt.val(); 
-
-    var txtToAdd = balise+"  "+finbalise; 
-
-    var x = document.createElement("INPUT");
- 
-    var button = document.createElement("button");
-
-    var input = document.createElement("INPUT");
-input.setAttribute("type", "text");
-
-    var div = document.createElement("div");
-
-    var div1 = document.createElement("div");
-
-    var div2 = document.createElement("div");
-
-    idinputtext = "text"+idinput;
-    
-     input.setAttribute("type", "text");
-
-     var f = 'd1';
-
-     var t = document.createElement("button");
-
-      input.setAttribute("id", idinputtext);
-
-     t.setAttribute("id",idinput);
-
-     t.innerText = "souligne";
-
- 
-   div.append(div1);
-
-   div1.append(t);
-
-   $txt.append(div);
-
-   $txt.append(div2);
-
-   div2.append(input);
-   
-    var qinput = "#"+idinput;
-
-     $("#"+idinput).bind("click", function() {
-
-add_class_css(this.id,"text-decoration-underline");
-
-});
-    
-
-}
-
-
-
-
 </script>
 
 <script type="text/javascript"  src = "./js/add_class_css.js" > </script>
+
+<script type="text/javascript"  src = "./js/add_balise.js" > </script>
+
+
 
 </body>
 </html>
